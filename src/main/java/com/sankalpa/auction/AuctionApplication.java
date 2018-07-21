03458d04536d2ac402e9d623e1de816be7850613
Aashutosh Poudel <aashutsohpoudyal@gmail.com>
2018-07-21 23:10:35 +0545

    file_upload

--- src/main/java/com/sankalpa/auction/AuctionApplication.java
+++ src/main/java/com/sankalpa/auction/AuctionApplication.java
@@ -1,11 +1,14 @@
 package com.sankalpa.auction;
 
+import com.sankalpa.auction.Util.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class AuctionApplication {
 
 	@Bean
---
+++ src/main/java/com/sankalpa/auction/Util/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.sankalpa.auction.Util;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
