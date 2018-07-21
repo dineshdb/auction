01573003385b1ccad4f4911b2a82a9f02b70f7d7
Dinesh Bhattarai <dbhattarai252@gmail.com>
2018-07-21 23:26:27 +0545

    Add cors support

--- src/main/java/com/sankalpa/auction/controller/UserController.java
+++ src/main/java/com/sankalpa/auction/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/users")
 public class UserController {
 
