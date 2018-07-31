e708e42dc255ad0b5749a3abc43a9fb5d1884d85
Devi Bhattarai <devakabhattarai327@gmail.com>
2018-07-31 22:55:11 +0545

    Merge conflict

--- src/main/java/com/sankalpa/auction/controller/AuctionController.java
+++ src/main/java/com/sankalpa/auction/controller/AuctionController.java
@@ -3,62 +3,80 @@ package com.sankalpa.auction.controller;
 import com.sankalpa.auction.Holder.AuctionHolder;
 import com.sankalpa.auction.model.Auction;
 import com.sankalpa.auction.model.Bid;
+import com.sankalpa.auction.model.User;
 import com.sankalpa.auction.service.AuctionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/auctions")
 public class AuctionController {
 
     @Autowired
     private AuctionService auctionService;
 
-    @GetMapping("/auctions")
+    @GetMapping("/")
     public @ResponseBody List<Long> getAllAuctionIds(){
         return auctionService.getAllAuctionIds();
     }
 
-    @GetMapping("/auctions/{auctionId}")
-    public @ResponseBody Auction getAuction(@PathVariable Long auctionId){
-        return auctionService.getAuction(auctionId);
-    }
-
-    @PostMapping("/auctions")
+    @PostMapping("/")
     public @ResponseBody Auction addAuction(@RequestBody Auction newAuction){
         return auctionService.addAuction(newAuction);
     }
 
-    @PutMapping("/auctions")
+    @GetMapping("/{auctionId}")
+    public @ResponseBody Auction getAuction(@PathVariable Long auctionId){
+        return auctionService.getAuction(auctionId);
+    }
+
+    @PutMapping("/")
     public @ResponseBody Auction updateAuction(@RequestBody Auction updatedAuction){
         return auctionService.updateAuction(updatedAuction);
     }
 
-    @DeleteMapping("/auctions/{auctionId}")
+    @DeleteMapping("/{auctionId}")
     public void deleteAuction(@PathVariable Long auctionId){
         auctionService.deleteAuction(auctionId);
     }
 
-    @PostMapping("/auctions/createAuction")
+    @PostMapping("/createAuction")
     public Auction createAcution(@RequestBody AuctionHolder auctionHolder){
         Auction auction = auctionService.createAuction(auctionHolder);
         //auctionService.schedule(auction);
         return auction;
     }
 
-    @GetMapping("/auctions/{auctionId}/participate/{bidderId}")
-    public void participate(@PathVariable("auctionId") Long auctionId, @PathVariable("bidderId") Long bidderId){
+    @Deprecated
+    @GetMapping("/{auctionId}/participate")
+    public void participate(@PathVariable("auctionId") Long auctionId, Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        Long bidderId = user.getUserId();
         auctionService.participate(auctionId, bidderId);
     }
 
-    @GetMapping("/auctions/{auctionId}/unsubscribe/{bidderId}")
+    @Deprecated
+    @GetMapping("/{auctionId}/unsubscribe/{bidderId}")
     public void unsubscribe(@PathVariable("auctionId") Long auctionId, @PathVariable("bidderId") Long bidderId){
         auctionService.unsubscribe(auctionId, bidderId);
     }
 
-    @GetMapping("/auctions/{auctionId}/bids")
+    @GetMapping("/{auctionId}/bids")
     public List<Bid> allBids(@PathVariable Long auctionId){
         return auctionService.allBids(auctionId);
+    }
+
+    @GetMapping("/{auctionId}/favorite")
+    public void favorite(@PathVariable Long auctionId, Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        auctionService.participate(auctionId, user.getUserId());
+    }
+    @GetMapping("/{auctionId}/unfavorite")
+    public void unfavorite(@PathVariable Long auctionId, Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        auctionService.unsubscribe(auctionId, user.getUserId());
     }
 }
